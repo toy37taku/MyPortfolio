8a402aad28f8a2ef142b2ca94a688698aa99f267
toy37tak <0037.toyooka.takumi@gmail.com>
2024-07-29 14:32:11 +0900

    demo

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return '<h1>ハローフラスコ</h1>'
-
-if __name__ == "__main__":
-    app.run()
+   return "<p>Hello, World!</p>"
