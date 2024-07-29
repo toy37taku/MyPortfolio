3b0e0afb3b483fb0c5602d56eea6dea835b4dcde
toy37tak <toy.t.2024@gmail.com>
2024-07-29 23:01:42 +0900

    １１１

--- webshop/app.py
+++ webshop/app.py
@@ -18,7 +18,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # データファイルのパス
-DATA_FILE = 'MyPortfolio/webshop/data/products.json'
+DATA_FILE = 'data/products.json'
 
 # JSONデータを読み込む関数
 def get_json_data():
