6ccd9467418ff8efd0e8371ff35daf6a95a9eeff
toy37tak <0037.toyooka.takumi@gmail.com>
2024-07-31 13:27:07 +0900

    a

--- webshop/app.py
+++ webshop/app.py
@@ -24,7 +24,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-DATA_FILE = 'static/data/products.json'
+DATA_FILE = '/webshop/static/data/products.json'
 
 # JSONデータを読み込む関数
 def get_json_data():
